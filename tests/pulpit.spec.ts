b30939ee0bb118dd55367255a552928b72871bab
MRGuziX <tomasz.guzik@onet.eu>
2023-08-03 16:23:15 +0200

    added test

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -33,12 +33,12 @@ test.describe(' Pulpit tests ', () => {
   });
 
   test('mobile money transfer', async ({ page }) => {
-    const topupReceiver = '500 xxx xxx';
-    const topupAmout = '10';
-    const expectedTopupMessage = `Doładowanie wykonane! ${topupAmout},00PLN na numer ${topupReceiver}`;
+    const topUpReceiver = '500 xxx xxx';
+    const topUpAmount = '10';
+    const expectedTopUpMessage = `Doładowanie wykonane! ${topUpAmount},00PLN na numer ${topUpReceiver}`;
 
-    await page.locator('#widget_1_topup_receiver').selectOption(topupReceiver);
-    await page.locator('#widget_1_topup_amount').fill(topupAmout);
+    await page.locator('#widget_1_topup_receiver').selectOption(topUpReceiver);
+    await page.locator('#widget_1_topup_amount').fill(topUpAmount);
     await page.locator('#widget_1_topup_agreement').check();
 
     await expect(
@@ -49,7 +49,7 @@ test.describe(' Pulpit tests ', () => {
     await page.getByTestId('close-button').click();
 
     await expect(page.getByTestId('message-text')).toHaveText(
-      expectedTopupMessage,
+      expectedTopUpMessage,
     );
   });
 
@@ -68,11 +68,11 @@ test.describe(' Pulpit tests ', () => {
       requiredFieldWarning,
     );
 
-    const topupReceiver = '500 xxx xxx';
-    const topupAmount = '10';
+    const topUpReceiver = '500 xxx xxx';
+    const topUpAmount = '10';
 
-    await page.locator('#widget_1_topup_receiver').selectOption(topupReceiver);
-    await page.locator('#widget_1_topup_amount').fill(topupAmount);
+    await page.locator('#widget_1_topup_receiver').selectOption(topUpReceiver);
+    await page.locator('#widget_1_topup_amount').fill(topUpAmount);
     await page.locator('#widget_1_topup_agreement').check();
 
     await expect(
@@ -88,64 +88,79 @@ test.describe(' Pulpit tests ', () => {
   });
 
   test('mobile top-up amount warning test', async ({ page }) => {
-    const topupReceiver = '500 xxx xxx';
-    let topupAmount = '1';
+    const topUpReceiver = '500 xxx xxx';
+    let topUpAmount = '1';
     const amountIsHigherThan5 = 'kwota musi być większa lub równa 5';
 
-    await page.locator('#widget_1_topup_receiver').selectOption(topupReceiver);
-    await page.locator('#widget_1_topup_amount').fill(topupAmount);
+    await page.locator('#widget_1_topup_receiver').selectOption(topUpReceiver);
+    await page.locator('#widget_1_topup_amount').fill(topUpAmount);
     await page.locator('#widget_1_topup_amount').blur();
 
     await expect
       .soft(page.getByTestId('error-widget-1-topup-amount'))
       .toHaveText(amountIsHigherThan5);
 
-    topupAmount = '5';
+    topUpAmount = '5';
 
-    await page.locator('#widget_1_topup_amount').fill(topupAmount);
+    await page.locator('#widget_1_topup_amount').fill(topUpAmount);
     await page.locator('#widget_1_topup_amount').blur();
 
     await expect
       .soft(page.getByTestId('error-widget-1-topup-amount'))
       .toBeHidden();
 
-    topupAmount = '150';
+    topUpAmount = '150';
 
-    await page.locator('#widget_1_topup_amount').fill(topupAmount);
+    await page.locator('#widget_1_topup_amount').fill(topUpAmount);
     await page.locator('#widget_1_topup_amount').blur();
 
     await expect
       .soft(page.getByTestId('error-widget-1-topup-amount'))
       .toBeHidden();
 
-    topupAmount = '151';
+    topUpAmount = '151';
     const amountIsHigherThan150 = 'kwota musi być mniejsza lub równa 150';
 
-    await page.locator('#widget_1_topup_amount').fill(topupAmount);
+    await page.locator('#widget_1_topup_amount').fill(topUpAmount);
     await page.locator('#widget_1_topup_amount').blur();
 
     await expect
       .soft(page.getByTestId('error-widget-1-topup-amount'))
       .toHaveText(amountIsHigherThan150);
 
-    topupAmount = '50,01';
+    topUpAmount = '50,01';
     const amountWithoutPennies = 'kwota musi być bez groszy';
 
-    await page.locator('#widget_1_topup_amount').fill(topupAmount);
+    await page.locator('#widget_1_topup_amount').fill(topUpAmount);
     await page.locator('#widget_1_topup_amount').blur();
 
     await expect
       .soft(page.getByTestId('error-widget-1-topup-amount'))
       .toHaveText(amountWithoutPennies);
 
-    topupAmount = '50,,01';
+    topUpAmount = '50,,01';
     const amountIsIncorrect = 'podana kwota jest niepoprawna';
 
-    await page.locator('#widget_1_topup_amount').fill(topupAmount);
+    await page.locator('#widget_1_topup_amount').fill(topUpAmount);
     await page.locator('#widget_1_topup_amount').blur();
 
     await expect
       .soft(page.getByTestId('error-widget-1-topup-amount'))
       .toHaveText(amountIsIncorrect);
+  });
+
+  test('correct balance after sucessful mobile top-up', async ({ page }) => {
+    const topUpReceiver = '500 xxx xxx';
+    const topUpAmount = '10';
+    const initialBalance = await page.locator('#money_value').innerText();
+    const expectedBalance = Number(initialBalance) - Number(topUpAmount);
+
+    await page.locator('#widget_1_topup_receiver').selectOption(topUpReceiver);
+    await page.locator('#widget_1_topup_amount').fill(topUpAmount);
+    await page.locator('#widget_1_topup_agreement').check();
+    await page.getByRole('button', { name: 'doładuj telefon' }).click();
+    await page.getByTestId('close-button').click();
+
+    await expect(page.locator('#money_value')).toHaveText(`${expectedBalance}`);
   });
 });
